0cab28e4c435050e96ee1522ce300b5b73680ec8
rurudo <ohs10587.2012@gmail.com>
2015-11-23 05:12:18 +0000

    Fix install path

---
+++ roles/ansible-role-fzf/files/fzf.zsh
@@ -0,0 +1,7 @@
+eh() {
+  eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
+}
+
+fh() {
+  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
+}
